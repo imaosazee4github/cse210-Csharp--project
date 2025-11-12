a847006e14dcce61cbb1b590aedb0e7f0f776758
imaosazee <imaosazee@gmail.com>
2025-11-12 10:34:31 +0100

    updated for rubric corrections-addcommentstoprogram.cs

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -10,8 +10,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Create a new Journal object to hold entries
        Journal journal = new Journal();
 
+       // A list of prompts that encourage reflective journal writing
        List<string>prompts = new List<string>()
        {
         "Who was the most interesting person I interacted with today?",
@@ -23,6 +25,7 @@ class Program
 
        string myChoice = "";
 
+       // A menu-driven program allowing user interaction until they choose to quit
        while (myChoice != "5")
        {
         Console.WriteLine("Please select one of the following choices:");
@@ -36,40 +39,49 @@ class Program
 
         if (myChoice == "1")
         {
+         // Generate a random prompt to make journaling more engaging
             Random rand = new Random();
             string _promptText = prompts[rand.Next(prompts.Count)];
 
+        
             Console.WriteLine(_promptText);
             Console.Write(">");
             string entryText = Console.ReadLine();
 
+            // Additional feature: mood rating and it allows the user to reflect emotionally on their day activities
             Console.Write("Rate your mood today (1-10): ");
             int mood = int.Parse(Console.ReadLine());
 
+            // Create a new entry with all user input
             Entry newEntry = new Entry();
             newEntry._date = DateTime.Now.ToShortDateString();
             newEntry._promptText = _promptText;
             newEntry._entryText = entryText;
             newEntry._mood = mood;
-
+            
+            // Add the entry to the journal and immediately display updates
             journal.AddEntry(newEntry);
 
+            // Immediate feedback feature — shows new entry added
             Console.WriteLine("\nYour entry has been added. Here is your updated journal: \n");
             journal.DisplayAll();
         }
         else if (myChoice == "2")
         {
+        // Display all journal entries
             journal.DisplayAll();
         }
-
+        
         else if (myChoice == "3")
         {
+           // Save entries to a file 
             Console.Write("What is the filename?");
             string file = Console.ReadLine();
             journal.SaveToFile(file);
         }
         else if (myChoice == "4")
         {
+        // Load entries from a saved file
             Console.Write("What is the filename?");
             string file = Console.ReadLine();
             journal.LoadFromFile(file);
