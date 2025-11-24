aa5edf792302d5db36c6c16744f1aaceda54c67f
imaosazee <imaosazee@gmail.com>
2025-11-24 17:03:22 +0100

    W04 Assignment: Online Ordering Program

---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+
+public class Order
+{
+    private List<Product> _products = new List<Product>();
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double total = 0;
+
+        foreach (Product prod in _products)
+        {
+            total += prod.GetTotalCost();
+        }
+
+        //Shipping cost
+
+        if(_customer.LivesInUSA())
+        {
+            total += 5;
+        }
+        else
+        {
+            total += 35;
+        }
+
+        return total;
+    }
+
+    public string  GetPackingLabel()
+    {
+        string label = "PACKING:\n";
+        foreach(Product p in _products)
+        {
+            label += $"{p.GetName()} (ID: {p.GetProductId()})\n";
+        }
+        return label;
+    }
+    public string GetShippingLabel()
+    {
+    return $"SHIPPING LABEL:\n{_customer.GetName()}\n{_customer.GetAddress().GetFullAddress()}";
+    }
+}
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Console.WriteLine("Online Ordering Program\n");
+
+        //------ORDER1------
+
+        Address addr1 =  new Address("123 Go St", "New York", "NY", "USA");
+        Customer cus1 = new Customer("Kelvin Smith", addr1);
+        Order order1 = new Order(cus1);
+
+        order1.AddProduct(new Product("Laptop", 101, 1200, 1));
+        order1.AddProduct(new Product("Mouse", 102, 25, 2));
+
+        //-----ORDER 2-------
+        Address addr2 = new Address("45 Zine Road", "Toronto", "ON", "Canada");
+        Customer cus2 = new Customer("Mercy James", addr2);
+        Order order2 = new Order(cus2);
+
+        order2.AddProduct(new Product("Phone", 201, 800, 1));
+        order2.AddProduct(new Product("Charger", 202, 30, 3));
+
+        //Display Results
+        DisplayOrder(order1);
+        Console.WriteLine("--------------");
+        DisplayOrder(order2);
+    }
+
+    static void DisplayOrder(Order order)
+    {
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine("Total Cost: $" + order.GetTotalCost());
+        Console.WriteLine();
     }
 }
